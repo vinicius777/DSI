b02c27c0c84836b6a7bcdc1c0de93db31e386512
vinicius777 <viniciusguedes63@gmail.com>
2021-10-01 13:12:48 -0300

    inittial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,117 +7,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: RandomWords(),
+      title: "Possible Band Names",
+      home: Vote()
     );
   }
 }
-
-class RandomWords extends StatefulWidget {
+class Vote extends StatefulWidget {
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  _VoteState createState() => _VoteState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = [];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-  final changWord = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
+class _VoteState extends State<Vote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text("Possible Band Names"),
         centerTitle: true,
-        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
-      body: _buildSuggestions(),
     );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          return Divider();
-        }
-        final int index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return Dismissible(child: _buildRow(_suggestions[index]),
-            key: Key(_suggestions[index].toString()),
-            onDismissed: (direction){
-              setState(() {
-                _suggestions.removeAt(index);
-              });
-            },
-            background: Container(color: Colors.red));
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: IconButton(
-        icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border),
-        color: alreadySaved ? Colors.red : null,
-        onPressed: (){
-          setState(() {
-            if (alreadySaved){
-              _saved.remove(pair);
-            } else {
-              _saved.add(pair);
-            }
-          });
-        },
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final tiles = _saved.map(
-            (WordPair pair) {
-          return ListTile(
-            title: Text(
-              pair.asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        },
-      );
-      final divided = ListTile.divideTiles(
-        context: context,
-        tiles: tiles,
-      ).toList();
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Saved Suggestions'),
-        ),
-        body: ListView(children: divided),
-      );
-    }));
   }
 }
+
