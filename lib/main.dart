fb0abd51688bd3e7f23b0da95e5cb620b6c06e82
vinicius777 <viniciusguedes63@gmail.com>
2021-10-06 21:02:26 -0300

    alteração codigo aula

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,70 @@
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(InitialFirebase());
+}
+
+class InitialFirebase extends StatefulWidget {
+  const InitialFirebase({key}) : super(key: key);
+
+  @override
+  _InitialFirebaseState createState() => _InitialFirebaseState();
+}
+
+class _InitialFirebaseState extends State<InitialFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return _buildAppError(snapshot.error);
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return (MyApp());
+        }
+        return _Loading();
+      },
+    );
+  }
+}
+
+Directionality _buildAppError(Object error) {
+  return Directionality(
+    textDirection: TextDirection.ltr,
+    child: Center(
+      child: Wrap(
+        children: [
+          const Text('Error:', style: TextStyle(fontSize: 20.0)),
+          Text('$error', style: TextStyle(fontSize: 20.0)),
+        ],
+      ),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Band Name Survey",
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: const MyHomePage(title: 'Possible Band Names')
-    );
+        title: "Band Name Survey",
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: const MyHomePage(title: 'Possible Band Names'));
   }
 }
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key, this.title}) : super(key:key);
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document){
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return ListTile(
       title: Row(
         children: [
@@ -44,7 +86,7 @@ class MyHomePage extends StatelessWidget {
           )
         ],
       ),
-      onTap: (){
+      onTap: () {
         print("Should increase votes here");
       },
     );
@@ -58,19 +100,30 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('bandnames').snapshots(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData)return Text('Loading...');
-          return ListView.builder(
-            itemExtent: 80.0,
-            itemCount: snapshot.data.documents.lenght,
-            itemBuilder: (context, index)=>
-            _buildListItem(context, snapshot.data.documents[index]),
-          );
-        }
-
-      ),
+          stream:
+              FirebaseFirestore.instance.collection('bandnames').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return Text('Loading...');
+            return ListView.builder(
+              itemExtent: 80.0,
+              itemCount: snapshot.data.documents.lenght,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          }),
     );
   }
 }
 
+Widget _Loading() {
+  return Center(
+    child: Wrap(
+      alignment: WrapAlignment.center,
+      children: const [
+        CircularProgressIndicator(),
+        SizedBox(width: 10),
+        Text('Loading...', style: TextStyle(fontSize: 20.0)),
+      ],
+    ),
+  );
+}
